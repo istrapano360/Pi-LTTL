221ecc26552c4736cb32f230c330784daf3cecd6
istrapano360 <43506394+istrapano360@users.noreply.github.com>
2020-05-22 02:20:16 +0200

    Update restart_gdrive.py

--- restart_gdrive.py
+++ restart_gdrive.py
@@ -1,17 +1,11 @@
 import os
 import time
 from time import sleep
-#restard gdrive
+#restart gdrive
 
-print("unmontirati cu drive")
+print("I will unmount gdrive")
 sleep(1)
 os.system("fusermount -u gdrive")
 sleep(2)
-print("montirati cu drive")
+print("I will mount the gdrive")
 os.system("rclone mount rpi-gdrive: gdrive")
-
-
-
-    
-
-#[ -d "/home/pi/Desktop/Scenic/gdrive/Test" ] && echo "Directory /path/to/dir exists."
